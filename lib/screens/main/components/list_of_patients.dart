08fb722905748b972782f342d278830dabe2c5f2
RishabhAgarwal-2001 <45998880+RishabhAgarwal-2001@users.noreply.github.com>
2021-03-08 08:57:32 +0530

    List Search Feature Implemented

--- lib/screens/main/components/list_of_patients.dart
+++ lib/screens/main/components/list_of_patients.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:exercise_tracker_doctor/models/Patient.dart';
 import 'package:exercise_tracker_doctor/constants.dart';
 import 'package:exercise_tracker_doctor/screens/main/components/patient_card.dart';
+import 'dart:async';
+
+class Debouncer {
+  final int milliseconds;
+  VoidCallback action;
+  Timer _timer;
+
+  Debouncer({this.milliseconds});
+
+  run(VoidCallback action) {
+    if (null != _timer) {
+      _timer.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
+  }
+}
 
 class ListOfPatients extends StatefulWidget {
   const ListOfPatients({
@@ -13,7 +29,14 @@ class ListOfPatients extends StatefulWidget {
 }
 
 class _ListOfPatientsState extends State<ListOfPatients> {
+  final _debouncer = Debouncer(milliseconds: 500);
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  List<Patient> _filteredPatients = List();
+  @override
+  void initState() {
+    super.initState();
+    _filteredPatients = patients;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +64,20 @@ class _ListOfPatientsState extends State<ListOfPatients> {
                     SizedBox(width: 5,),
                     Expanded(
                       child: TextField(
-                        onChanged: (value) => print("Value = $value"), // TODO: When Value changes Apply Filter
+                        onChanged: (value) {
+                          _debouncer.run(
+                              () {
+                                setState((){
+                                  _filteredPatients = patients.where(
+                                          (p) => (
+                                          p.name.toLowerCase().contains(value.toLowerCase())
+                                      )
+                                  ).toList();
+                                  print(_filteredPatients);
+                                });
+                              }
+                          );
+                        },
                         decoration: InputDecoration(
                           hintText: "Search",
                           fillColor: kBgLightColor,
@@ -59,9 +95,9 @@ class _ListOfPatientsState extends State<ListOfPatients> {
               SizedBox(height: kDefaultPadding), // SizedBox
               Expanded(
                   child: ListView.builder(
-                    itemCount: patients==null ? 0: patients.length,
+                    itemCount: _filteredPatients==null ? 0: _filteredPatients.length,
                     itemBuilder: (context, index) => PatientCard(
-                        patient: patients[index],
+                        patient: _filteredPatients[index],
                         press:(){}
                     )
                   )
